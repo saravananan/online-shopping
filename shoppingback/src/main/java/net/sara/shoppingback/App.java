6bcd11e0036ad2af9da0a3495a0436f68463c7d1
saravananan <saravanakrishnasamy1@gmail.com>
2018-03-20 15:58:18 +0530

    my project setup completed.

---
+++ shoppingback/src/main/java/net/sara/shoppingback/App.java
@@ -0,0 +1,13 @@
+package net.sara.shoppingback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
